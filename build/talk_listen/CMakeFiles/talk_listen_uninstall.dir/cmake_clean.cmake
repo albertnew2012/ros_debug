6eea60b1f77c08e7ba8aafc737c2a288e2c13624
albertl <albertl@safeai.ai>
2024-07-03 17:55:02 -0700

    add .gitignore

--- build/talk_listen/CMakeFiles/talk_listen_uninstall.dir/cmake_clean.cmake
+++
@@ -1,8 +0,0 @@
-file(REMOVE_RECURSE
-  "CMakeFiles/talk_listen_uninstall"
-)
-
-# Per-language clean rules from dependency scanning.
-foreach(lang )
-  include(CMakeFiles/talk_listen_uninstall.dir/cmake_clean_${lang}.cmake OPTIONAL)
-endforeach()
--- build/talk_listen/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /home/albertl/Desktop/ros_debug/talk_listen
-# Build directory: /home/albertl/Desktop/ros_debug/build/talk_listen
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
